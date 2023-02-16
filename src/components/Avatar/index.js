59d140505ff0b2343b29a6ba1c260c8ba988f522
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-02-16 16:32:10 -0300

    H-05: create avatar component

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./avatar.module.css";
+export default function Avatar({ alt, src, text }) {
+  return (
+    <div className={styles.avatarContainer}>
+      <Image
+        className={styles.avatar}
+        loader={() => src}
+        unoptimized={true}
+        src={src}
+        width={49}
+        height={49}
+        alt={alt}
+      />
+      {text && <p>{text}</p>}
+    </div>
+  );
+}
