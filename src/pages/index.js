6c61089977de4f02c22d6d9fda1e9ef0dff98a8e
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-02-16 16:34:44 -0300

    H-06: add new avatar component

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,12 +11,12 @@ import {
   singOutWithGoogle,
 } from "@/helpers/firebase";
 import { useEffect, useState } from "react";
+import Avatar from "@/components/Avatar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [user, setUser] = useState(null);
-  console.log(user);
   const handleLogin = () => {
     loginWithGoogle()
       .then(setUser)
@@ -39,30 +39,28 @@ export default function Home() {
         <link rel="icon" href="/caduceo.png" />
       </Head>
       <main className={styles.main}>
-        <Image className={styles.logo} src={logo} alt="caduceo" />
-        <h1 className={styles.title}>Welcome to Salute !</h1>
-        <h2>Talk about the lastest healthcare discoveries ...</h2>
-        {user === null && (
-          <Button onClick={handleLogin}>
-            <Image className={styles.google} src={google} alt="google" />
-            Login With Google
-          </Button>
-        )}
-        {user && user.avatar && (
-          <div>
-            <Image
-              className={styles.user}
-              loader={() => user.avatar}
-              unoptimized={true}
-              src={user.avatar}
-              width={50}
-              height={50}
-              alt="user"
-            />
-            <p>Hello {user.username}</p>
-            {/*<Button onClick={handleSingOut}>Sing Out</Button>}*/}
-          </div>
-        )}
+        <Image className={styles.logo} src={logo} alt="caduceo" priority />
+        <div className={styles.presentation}>
+          <h1 className={styles.title}>Welcome to Salute !</h1>
+          <p>Talk about the lastest healthcare discoveries ...</p>
+
+          {user === null && (
+            <Button onClick={handleLogin}>
+              <Image className={styles.google} src={google} alt="google" />
+              Login With Google
+            </Button>
+          )}
+          {user && user.avatar && (
+            <div className={styles.user}>
+              <Avatar
+                alt={user.username}
+                src={user.avatar}
+                text={user.username}
+              />
+              <Button onClick={handleSingOut}>Sing Out</Button>
+            </div>
+          )}
+        </div>
       </main>
     </>
   );
