c66a688ec9ae0aab62a1670e919a426935e86068
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-02-18 23:12:24 -0300

    H-22: add disabled functionality

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,8 @@
 import styles from "@/components/Button/button.module.css";
-export default function Button({ children, onClick }) {
+export default function Button({ children, onClick, disabled }) {
   return (
     <>
-      <button className={styles.btn} onClick={onClick}>
+      <button className={styles.btn} onClick={onClick} disabled={disabled}>
         {children}
       </button>
     </>
