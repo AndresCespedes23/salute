180e56a102ec5ab768d379c7f3809b25f6d22b7f
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-02-18 23:10:12 -0300

    H-21: create a function to handle the user hook

---
+++ src/hooks/useUser.js
@@ -0,0 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import { onAuthStateChange } from "../helpers/firebase";
+import { useRouter } from "next/router";
+
+export const USER_STATES = {
+  NOT_LOGGED: null,
+  NOT_KNOWN: undefined,
+};
+
+export default function useUser() {
+  const [user, setUser] = useState(USER_STATES.NOT_KNOWN);
+  const router = useRouter();
+
+  useEffect(() => {
+    onAuthStateChange(setUser);
+  }, []);
+
+  useEffect(() => {
+    user === USER_STATES.NOT_LOGGED && router.push("/");
+  }, [user]);
+
+  return user;
+}
