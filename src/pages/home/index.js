28b0b116bb90f4f7feb50b305d9d7d8073d7d799
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-02-20 19:08:43 -0300

    H-24: add fetch latest tweets functionality with firebase

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,6 @@
 import HtmlHead from "@/components/HtmlHead";
 import Tweet from "@/components/Tweet";
+import { fetchLatestTweets } from "@/helpers/firebase";
 import useUser from "@/hooks/useUser";
 import { useEffect, useState } from "react";
 import styles from "./home.module.css";
@@ -8,10 +9,7 @@ export default function Home() {
   const [timeline, setTimeline] = useState([]);
   const user = useUser();
   useEffect(() => {
-    user &&
-      fetch("/api/statuses/home_timeline")
-        .then((res) => res.json())
-        .then(setTimeline);
+    user && fetchLatestTweets().then(setTimeline);
   }, [user]);
   return (
     <>
@@ -20,13 +18,13 @@ export default function Home() {
         <h2 className={styles.homeTitle}>Inicio</h2>
       </header>
       <section className={styles.homeTimeline}>
-        {timeline.map(({ id, username, avatar, message }) => (
+        {timeline.map(({ id, tweet }) => (
           <Tweet
             id={id}
             key={id}
-            avatar={avatar}
-            username={username}
-            message={message}
+            avatar={tweet.avatar}
+            userName={tweet.userName}
+            content={tweet.content}
           />
         ))}
       </section>
