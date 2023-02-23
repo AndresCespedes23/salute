ed736dbf8ab62947debc22f24534d0421254ca0b
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-02-23 17:29:47 -0300

    H-34: add upload img functionality

--- src/pages/compose/tweet/index.js
+++ src/pages/compose/tweet/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import HtmlHead from "@/components/HtmlHead";
 import Button from "@/components/Button";
 import styles from "./tweet.module.css";
@@ -5,8 +7,12 @@ import useUser from "@/hooks/useUser";
 import { useState } from "react";
 import { addTweet } from "../../../helpers/firebase";
 import { useRouter } from "next/router";
+import Avatar from "@/components/Avatar";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-//This states are to prevent the users for submit the same twit multiple times
+const storage = getStorage();
+
+//This states are to prevent the users for submit the same tweet multiple times
 const COMPOSE_STATES = {
   USER_NOT_KNOWN: 0,
   LOADING: 1,
@@ -14,11 +20,24 @@ const COMPOSE_STATES = {
   ERROR: -1,
 };
 
+const DRAG_IMAGE_STATES = {
+  ERROR: -1,
+  NONE: 0,
+  DRAG_OVER: 1,
+  UPLOADING: 2,
+  COMPLETE: 3,
+};
+
 export default function ComposeTweet() {
   const user = useUser();
   const router = useRouter();
+
   const [message, setMessage] = useState("");
   const [status, setStatus] = useState(COMPOSE_STATES.USER_NOT_KNOWN);
+
+  const [drag, setDrag] = useState(DRAG_IMAGE_STATES.NONE);
+  const [imgURL, setImgURL] = useState(null);
+
   const handleChange = (e) => {
     const { value } = e.target;
     setMessage(value);
@@ -31,6 +50,7 @@ export default function ComposeTweet() {
       userName: user.username,
       avatar: user.avatar,
       content: message,
+      img: imgURL,
     })
       .then(() => {
         router.push("/home");
@@ -40,20 +60,68 @@ export default function ComposeTweet() {
         setStatus(COMPOSE_STATES.ERROR);
       });
   };
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    setDrag(DRAG_IMAGE_STATES.DRAG_OVER);
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setDrag(DRAG_IMAGE_STATES.NONE);
+  };
+
+  const handleDrop = async (e) => {
+    e.preventDefault();
+    setDrag(DRAG_IMAGE_STATES.NONE);
+    const file = e.dataTransfer.files[0];
+    const storageRef = ref(storage, `images/${file.name}`);
+    await uploadBytes(storageRef, file);
+    const downloadUrl = await getDownloadURL(storageRef);
+    setImgURL(downloadUrl);
+  };
+
   const isButtonDisabled = !message.length || status === COMPOSE_STATES.LOADING;
+
   return (
     <>
       <HtmlHead />
-      <form onSubmit={handleSubmit}>
-        <textarea
-          onChange={handleChange}
-          className={styles.tweetArea}
-          placeholder="What's happening?"
-          value={message}
-        />
-        <div className={styles.btnContainer}>
-          <Button disabled={isButtonDisabled}>Tweet</Button>
-        </div>
+      <form className={styles.formTweet} onSubmit={handleSubmit}>
+        {user && (
+          <section className={styles.avatarContainer}>
+            <Avatar src={user.avatar} alt="usuario" />
+          </section>
+        )}
+        {drag === DRAG_IMAGE_STATES.DRAG_OVER ? (
+          <textarea
+            onChange={handleChange}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
+            className={styles.tweetAreaBorder}
+            placeholder="What's happening?"
+            value={message}
+          />
+        ) : (
+          <textarea
+            onChange={handleChange}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
+            className={styles.tweetArea}
+            placeholder="What's happening?"
+            value={message}
+          />
+        )}
+
+        {imgURL && (
+          <section>
+            <button className={styles.btnImg} onClick={() => setImgURL(null)}>
+              x
+            </button>
+            <img className={styles.uploadImg} src={imgURL} alt={imgURL} />
+          </section>
+        )}
+        <Button disabled={isButtonDisabled}>Tweet</Button>
       </form>
     </>
   );
