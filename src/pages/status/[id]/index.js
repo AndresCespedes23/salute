e3428b70e2271ce08f084e55fbf97fdc1481007b
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-02-24 18:50:13 -0300

    add page to get the tweet when it's clicked on the timeline

---
+++ src/pages/status/[id]/index.js
@@ -0,0 +1,10 @@
+import HtmlHead from "@/components/HtmlHead";
+
+export default function TweetPage() {
+  return (
+    <section>
+      <HtmlHead />
+      <div>asfadsg</div>
+    </section>
+  );
+}
