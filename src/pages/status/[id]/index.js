303e2e434536b2c519a2ea0c40464ddb52986166
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-02-28 12:05:03 -0300

    H-46: implement getStaticProps for the data fetch of the tweets

--- src/pages/status/[id]/index.js
+++ src/pages/status/[id]/index.js
@@ -1,10 +1,54 @@
 import HtmlHead from "@/components/HtmlHead";
+import Tweet from "@/components/Tweet";
+import admin from "@/helpers/firebase/admin";
+import { useRouter } from "next/router";
 
-export default function TweetPage() {
+export default function TweetPage(props) {
+  const router = useRouter();
+
+  if (router.isFallback) return <h1>Loading...</h1>;
   return (
     <section>
       <HtmlHead />
-      <div>asfadsg</div>
+      <Tweet {...props} />
     </section>
   );
+}
+
+export async function getStaticPaths() {
+  const snapshot = await admin.firestore().collection("tweets").get();
+
+  const paths = snapshot.docs.map((doc) => ({
+    params: { id: doc.id },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { id } = params;
+
+  return admin
+    .firestore()
+    .collection("tweets")
+    .doc(id)
+    .get()
+    .then((doc) => {
+      const data = doc.data();
+      const id = doc.id;
+      const { createdAt } = data;
+
+      const props = {
+        ...data.tweet,
+        id,
+        createdAt: +createdAt.toDate(),
+      };
+      return { props };
+    })
+    .catch(() => {
+      return { props: {} };
+    });
 }
