a7e2b0c17f6946186b232bf444dc8fc5a09b9d05
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-02-28 11:59:27 -0300

    H-44: add firebase admin to operate functions in the server side of the app

--- src/helpers/firebase/admin.js
+++ src/helpers/firebase/admin.js
@@ -1,10 +1,11 @@
 const admin = require("firebase-admin");
 
-if (admin.apps.lenght) {
-  const serviceAccount = require("./firebase-admin.json");
+const serviceAccount = require("./admin.json");
 
+try {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-}
-export const firestore = admin.firestore();
+} catch (e) {}
+
+export default admin;
