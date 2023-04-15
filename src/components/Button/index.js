ba91402f4bbc6b80cc60cae78046df869f0a1af7
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-04-15 18:17:29 -0300

    H-62: relocate all css files in the styles folder

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,4 +1,4 @@
-import styles from "@/styles/Button/button.module.css";
+import styles from "@/styles/button.module.css";
 export default function Button({ children, onClick, disabled }) {
   return (
     <>
